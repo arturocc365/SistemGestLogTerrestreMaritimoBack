b0d6c30a25d34e173da052d1d35f9bf2a8414d13
arturocc365 <arturocuadros365@gmail.com>
2022-04-05 22:07:35 +0200

    first commit

---
+++ src/main/java/com/arturo/demo/SistemGestLogTerrestreMaritimoBackApplication.java
@@ -0,0 +1,13 @@
+package com.arturo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemGestLogTerrestreMaritimoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemGestLogTerrestreMaritimoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturo/demo/SistemGestLogTerrestreMaritimoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemGestLogTerrestreMaritimoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
