dffa34a19f734dcb13e78b4d05ebc3503ba135c9
arturocc365 <arturocuadros365@gmail.com>
2022-04-11 15:13:38 -0500

    subida de jpa

--- src/test/java/com/arturo/demo/dao/ICountryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.arturo.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.arturo.demo.model.entity.Country;
-
-public interface ICountryDAO extends CrudRepository<Country, Long>{
-
-}
--- src/test/java/com/arturo/demo/dao/IOrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.arturo.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.arturo.demo.model.entity.OrderDetail;
-
-public interface IOrderDetailDAO extends CrudRepository<OrderDetail, Long>{
-
-}
